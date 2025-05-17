0581fc5de0db7812329ef781f9067024fb876c2c
MostafaMovahhed <thisismovahhed@gmail.com>
2025-05-17 17:32:22 +0330

    add sentry dependency to package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -9,16 +9,24 @@ let package = Package(
     products: [
         .library(
             name: "WiseTrackLib",
-            targets: ["WiseTrackLib"]
+            targets: ["WiseTrackLibWrapper"]
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.50.2")
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.0.0")
     ],
     targets: [
         .binaryTarget(
             name: "WiseTrackLib",
-            url: "https://github.com/wisetrack-io/ios-sdk/releases/download/1.0.1/WiseTrackLib.xcframework.zip",            checksum: "d99765e56629a288b255b16c3b17344084d983b33800c732dbaaa0a828a25284"
+            url: "https://github.com/wisetrack-io/ios-sdk/releases/download/1.0.1/WiseTrackLib.xcframework.zip",
+            checksum: "d99765e56629a288b255b16c3b17344084d983b33800c732dbaaa0a828a25284"
+        ),
+        .target(
+            name: "WiseTrackLibWrapper",
+            dependencies: [
+                "WiseTrackLib",
+                .product(name: "Sentry", package: "sentry-cocoa")
+            ]
         )
     ]
 )
