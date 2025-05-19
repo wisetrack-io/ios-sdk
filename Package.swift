b4ae7caf0a837c8d824599c6b8bba2853253641e
MostafaMovahhed <thisismovahhed@gmail.com>
2025-05-19 12:10:40 +0330

    embed sentry in framework and remove dependency

--- Package.swift
+++ Package.swift
@@ -9,24 +9,14 @@ let package = Package(
     products: [
         .library(
             name: "WiseTrackLib",
-            targets: ["WiseTrackLibWrapper"]
+            targets: ["WiseTrackLib"]
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.50.2")
     ],
     targets: [
         .binaryTarget(
             name: "WiseTrackLib",
-            url: "https://github.com/wisetrack-io/ios-sdk/releases/download/1.0.1/WiseTrackLib.xcframework.zip",
-            checksum: "d99765e56629a288b255b16c3b17344084d983b33800c732dbaaa0a828a25284"
-        ),
-        .target(
-            name: "WiseTrackLibWrapper",
-            dependencies: [
-                "WiseTrackLib",
-                .product(name: "Sentry", package: "sentry-cocoa")
-            ]
+            url: "https://github.com/wisetrack-io/ios-sdk/releases/download/1.0.10/WiseTrackLib.xcframework.zip",
+            checksum: "a2a5b46b61ec506d0390cd0b21d2f9ddf776b11248b98fafcb040fd6459aa982"
         )
     ]
 )
