ed407821a726b7298368c0d87e597e26f8ce7a35
MostafaMovahhed <thisismovahhed@gmail.com>
2025-05-19 11:37:46 +0330

    update sentry lib version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.0.0")
+        .package(url: "https://github.com/getsentry/sentry-cocoa.git", from: "8.50.2")
     ],
     targets: [
         .binaryTarget(
